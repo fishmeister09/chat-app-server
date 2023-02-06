f960ee8acb9747ac53ab98b85467f63a0b97d813
Arjun Sethi <63541672+fishmeister09@users.noreply.github.com>
2023-02-06 23:29:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+let users = [];
+var removeUser = (userId) =>
+  users.find((u, i) => u.ID == userId && users.splice(i, 1));
+
+io.on('connection', (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on('join_room', (data) => {
+    const { username, room } = data;
+    users.push({ ID: socket.id, username: username, room: room });
+    socket.join(room);
+    console.log(`User with ID: ${socket.id} joined room: ${room}`);
+  });
+
+  socket.on('send_message', (data) => {
+    socket.to(data.room).emit('receive_message', data);
+  });
+
+  socket.on('getusers', () => {
+    socket.emit('users', users);
+  });
+
+  socket.on('disconnect', () => {
+    removeUser(socket.id);
+    console.log('User Disconnected', socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log('SERVER RUNNING');
+});

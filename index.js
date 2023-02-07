69497a5f373dc9ba12da5fccf0cc08276fe12f0a
Arjun Sethi <63541672+fishmeister09@users.noreply.github.com>
2023-02-07 12:47:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-// const cors = require('cors');
+const cors = require('cors');
 const { Server } = require('socket.io');
 
-// const corsOpts = {
-//   origin: '*',
+const corsOpts = {
+  origin: 'https://chat-app-q9tg.vercel.app/',
 
-//   methods: ['GET', 'POST'],
+  methods: ['GET', 'POST'],
+};
+app.use(cors(corsOpts));
 
-//   allowedHeaders: ['Content-Type'],
-// };
-// app.use(cors(corsOpts));
-app.use(function (req, res, next) {
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    'https://chat-app-q9tg.vercel.app/'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type'
-  );
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
 app.get('/', (req, res) => {
   res.send('<h1>Hey Socket.io</h1>');
 });
